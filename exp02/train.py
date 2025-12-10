17c6433091832306c300f2e0f9e9450be3f3db2b
mwh <1372942864@qq.com>
2025-12-10 16:23:59 +0800

    readme-exp02

--- exp02/train.py
+++ exp02/train.py
@@ -48,7 +48,7 @@ class MedicalImageDataset(Dataset):
 transform = transforms.Compose([
     transforms.Resize((224, 224)),  # 改为 224
     transforms.ToTensor(),
-    # ... 你的其他增强 ...
+    # ... 其他增强 ...
     # 使用标准的 ImageNet 归一化参数，配合预训练模型效果最好
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
